faf00c3f021c839645d846bf3d17b1cc0d76f11b
Yuval Goldberg <yuvigoldi@hotmail.com>
2020-07-15 20:02:51 +0300

    UpdateCluster receives parameters and returns a Cluster

--- internal/cluster/transition.go
+++ internal/cluster/transition.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/filanov/bm-inventory/internal/common"
 	logutil "github.com/filanov/bm-inventory/pkg/log"
 	"github.com/filanov/stateswitch"
 	"github.com/go-openapi/strfmt"
@@ -40,8 +39,8 @@ func (th *transitionHandler) PostCancelInstallation(sw stateswitch.StateSwitch, 
 	if sCluster.srcState == clusterStatusError {
 		return nil
 	}
-	return updateCluster(logutil.FromContext(params.ctx, th.log), sCluster.srcState,
-		params.reason, sCluster.cluster, params.db)
+	return updateClusterStatus(logutil.FromContext(params.ctx, th.log), params.db, *sCluster.cluster.ID, sCluster.srcState,
+		*sCluster.cluster.Status, params.reason)
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -63,8 +62,8 @@ func (th *transitionHandler) PostResetCluster(sw stateswitch.StateSwitch, args s
 	if !ok {
 		return errors.New("PostResetCluster invalid argument")
 	}
-	return updateCluster(logutil.FromContext(params.ctx, th.log), sCluster.srcState,
-		params.reason, sCluster.cluster, params.db)
+	return updateClusterStatus(logutil.FromContext(params.ctx, th.log), params.db, *sCluster.cluster.ID, sCluster.srcState,
+		*sCluster.cluster.Status, params.reason)
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -84,8 +83,8 @@ func (th *transitionHandler) PostPrepareForInstallation(sw stateswitch.StateSwit
 	if !ok {
 		return errors.New("PostResetCluster invalid argument")
 	}
-	return updateClusterStateWithParams(logutil.FromContext(params.ctx, th.log), sCluster.srcState,
-		statusInfoPreparingForInstallation, sCluster.cluster, th.db,
+	return updateClusterStatus(logutil.FromContext(params.ctx, th.log), th.db, *sCluster.cluster.ID, sCluster.srcState,
+		*sCluster.cluster.Status, statusInfoPreparingForInstallation,
 		"install_started_at", strfmt.DateTime(time.Now()))
 }
 
@@ -109,8 +108,9 @@ func (th *transitionHandler) PostCompleteInstallation(sw stateswitch.StateSwitch
 		return errors.New("PostCompleteInstallation invalid argument")
 	}
 
-	return updateClusterStateWithParams(logutil.FromContext(params.ctx, th.log), sCluster.srcState,
-		params.reason, sCluster.cluster, th.db, "install_completed_at", strfmt.DateTime(time.Now()))
+	return updateClusterStatus(logutil.FromContext(params.ctx, th.log), th.db, *sCluster.cluster.ID, sCluster.srcState,
+		*sCluster.cluster.Status, params.reason,
+		"install_completed_at", strfmt.DateTime(time.Now()))
 }
 
 func (th *transitionHandler) isSuccess(stateSwitch stateswitch.StateSwitch, args stateswitch.TransitionArgs) (b bool, err error) {
@@ -121,12 +121,4 @@ func (th *transitionHandler) isSuccess(stateSwitch stateswitch.StateSwitch, args
 func (th *transitionHandler) notSuccess(stateSwitch stateswitch.StateSwitch, args stateswitch.TransitionArgs) (b bool, err error) {
 	params, _ := args.(*TransitionArgsCompleteInstallation)
 	return !params.isSuccess, nil
-}
-
-// Updates the status according to the cluster where the status equals the srcStatus
-func updateCluster(log logrus.FieldLogger, srcStatus, statusInfo string, c *common.Cluster, db *gorm.DB,
-	extra ...interface{}) error {
-	newStatus := c.Status
-	c.Status = &srcStatus
-	return updateClusterStateWithParams(log, *newStatus, statusInfo, c, db, extra...)
 }
